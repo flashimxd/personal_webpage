af09b96e0c81f6f40071e18cd8bda8b1d0f23a2e
Rangel Netto <rangel@juno.com.br>
2020-03-02 18:46:01 -0300

    Clean up starter gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rangel Netto`,
+    description: `What is love? baby dont hurt me T.T`,
+    author: `@RangelNetto`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
